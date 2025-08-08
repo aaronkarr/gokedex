5613d8d7ade81f2eb1faf635e2eb8e917ba3304f
Aaron Karr <aaron@aaronkarr.com>
2025-08-08 11:24:24 -0400

    repl with initial function to split strings into words, testing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aaronkarr/gokedex
+
+go 1.24.5
---
+++ repl_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    " A 2 c D",
+			expected: []string{"a", "2", "c", "d"},
+		},
+		{
+			input:    "I am stuck in this struct",
+			expected: []string{"i", "am", "stuck", "in", "this", "struct"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths don't match: '%v' != '%v'", actual, c.expected)
+			continue
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
+			}
+		}
+	}
+}
